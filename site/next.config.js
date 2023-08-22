763283b00cd80b2817398ecd06b063fde238f25d
theroadrunn3r <142924878+theroadrunn3r@users.noreply.github.com>
2023-08-22 22:20:24 +0530

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Kasm Aorus',
+    description: 'The unofficial store for Kasm supported workspaces by Aorus.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://theroadrunn3r.github.io/kasm-registry/',
+    contactUrl: 'https://theroadrunn3r.github.io',
   },
   reactStrictMode: true,
   swcMinify: true,
