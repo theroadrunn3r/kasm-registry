eee094e033fb681123b583e86b821e5878f4a4f2
theroadrunn3r <142924878+theroadrunn3r@users.noreply.github.com>
2023-08-22 22:27:24 +0530

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/kasm-registry2/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
